8b868c5c1c0347c1f1a202c678c5ff1f3daddb97
Charlie Hawker <charlie.hawker@staffordlong.co.uk>
2016-07-07 10:31:17 +0100

    Changes overrides to use Deface DSL instead of Deface::Override

--- app/overrides/add_mailchimp_to_admin_sidebar_menu.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
-  name: 'mailchimp_admin_sidebar_menu',
-  insert_bottom: '#main-sidebar',
-  partial: 'spree/admin/shared/mailchimp_sidebar_menu'
-)
