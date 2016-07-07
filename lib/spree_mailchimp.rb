57c90ad2c676c11df2b1cf96ec868fc601062ef7
Charlie Hawker <charlie.hawker@staffordlong.co.uk>
2016-07-07 16:36:31 +0100

    DRY up code

--- lib/spree_mailchimp.rb
+++ lib/spree_mailchimp.rb
@@ -17,7 +17,7 @@ module SpreeMailchimp
     # TODO do not fail silently
     def subscribe(user)
       begin
-        @gibbon.lists(Spree::Config[:mailchimp_list_id]).members.create(body: {email_address: user.email, status: "subscribed"})
+        @gibbon.lists(Spree::Config[:mailchimp_list_id]).members.create(body: subscriber_data(user))
       rescue
       end
     end
@@ -25,7 +25,7 @@ module SpreeMailchimp
     # TODO do not fail silently
     def update(user)
       begin
-        @gibbon.lists(Spree::Config[:mailchimp_list_id]).members.upsert(body: {email_address: user.email, status: "subscribed"})
+        @gibbon.lists(Spree::Config[:mailchimp_list_id]).members(member_hash(user)).upsert(body: subscriber_data(user))
       rescue
       end
     end
@@ -34,13 +34,14 @@ module SpreeMailchimp
     # TODO do not fail silently
     def unsubscribe(user)
       begin
-        @gibbon.lists(Spree::Config[:mailchimp_list_id]).members(Digest::MD5.hexdigest(user.email.downcase)).update(body: { status: "unsubscribed" })
+        @gibbon.lists(Spree::Config[:mailchimp_list_id]).members(member_hash(user)).update(body: { status: "unsubscribed" })
       rescue
       end
     end
 
     private
 
+    # Check the mailchimp configuration variables are set
     def mailchimp_configured?
       !(Spree::Config[:mailchimp_api_key].nil? ||
         Spree::Config[:mailchimp_api_key].empty? ||
@@ -48,5 +49,25 @@ module SpreeMailchimp
         Spree::Config[:mailchimp_list_id].empty?
       )
     end
+
+    # Member hash for existing subscribers
+    def member_hash(user)
+      Digest::MD5.hexdigest(user.email.downcase)
+    end
+
+    # Merge fields for a user
+    def merge_fields(user)
+      unless user.bill_address.nil?
+        {FNAME: user.bill_address.firstname, LNAME: user.bill_address.lastname}
+      else
+        {}
+      end
+    end
+
+    # The data for a subscriber
+    def subscriber_data(user)
+      {email_address: user.email, merge_fields: merge_fields(user), status: "subscribed"}
+    end
+
   end
 end
