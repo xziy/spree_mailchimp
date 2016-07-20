6649533cc68140e5b09f0c69cb0d2ecb8c5e918f
xziy <cziy@ya.ru>
2016-07-20 21:53:55 +0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Spree::Core::Engine.add_routes do
 
-  namespace :admin, path: Spree.admin_path do
+  namespace :admin do
     get '/mailchimp', to: 'mailchimp#edit', as: :edit_mailchimp
     put '/mailchimp', to: 'mailchimp#update', as: :update_mailchimp
   end
 
 end
+
+
